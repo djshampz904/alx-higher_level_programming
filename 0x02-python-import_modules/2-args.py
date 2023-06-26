a3b3d803950f4a4d04f972b7c5a760f7e448d0cd
djshampz904 <djshampz05@gmail.com>
2023-06-26 15:02:11 +0000

    $message

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print('{} arguments.'.format(length))
     elif length == 1:
         print('{} argument:'.format(length))
-        print('{}: {}'.format(length, argv[1]))
+        print('{}:{}'.format(length, argv[1]))
     else:
         print('{} arguments: '.format(length))
         for i in range(1, length + 1):
-            print('{}: {}'.format(i, argv[i]))
+            print('{}:{}'.format(i, argv[i]))
