eb6820c51a8a870a98e6ae56d366d2114e88398b
djshampz904 <djshampz05@gmail.com>
2023-07-01 15:39:53 +0000

    pycodestyle

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
     temp = []
     for i in my_list:
         if i % 2 == 0:
-           temp.append(True)
+            temp.append(True)
         else:
             temp.append(False)
     return temp
