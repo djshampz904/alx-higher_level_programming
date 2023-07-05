a95ed9ccf5818e89cbde810a25573123004115b4
djshampz904 <djshampz05@gmail.com>
2023-07-05 12:54:36 +0000

    white space

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,10 +2,11 @@
 def multiply_by_2(a_dictionary):
     mydict = a_dictionary.copy()
     mykeys = []
+
     for key in mydict:
         mykeys.append(key)
 
     for key in mykeys:
         mydict[key] = mydict[key] * 2
-    return(mydict)
 
+    return(mydict)
