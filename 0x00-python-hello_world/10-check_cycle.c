bc7373c96f808a9a35b5f3b49efb634bea7c6fe4
martin-maverick <97390728+djshampz904@users.noreply.github.com>
2023-07-18 16:15:38 +0300

    Add files via upload

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-/**
- * check_cycle - check if list is a cycle
- * @list: pointer to list
- * Return: 0 or 1
- */
 
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle, *hare;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	if (list == NULL)
+	if (!list)
 		return (0);
 
-	hare = list;
-	turtle = list;
-	while (turtle && hare && hare->next)
+	while (slow && fast && fast->next)
 	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-
-		if (turtle == hare)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
 	}
+
 	return (0);
 }
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,9 +1,1 @@
-Running python script
-1 - run raw code using intepreter
-2 - python script printing a string
-3 - f strings
-Floating point 
-3 - repeating strings
-6 - concatination of strings
-7 - copy cut and paste
-9 zen of python
+0x00-python-hello_world
