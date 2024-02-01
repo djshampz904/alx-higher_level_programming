e29e788294bac05a15b76dc75e5078762607fa0d
djshampz904 <djshampz05@gmail.com>
2024-02-01 13:35:54 +0300

    more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+"""Module for Rectangle class"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/bin/usr/python3
+"""
+Creates a class Rectangle that defines a rectangle by:s
+"""
+
+
+class Rectangle:
+    __width = 0
+    __height = 0
+
+    def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute"""
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height attribute"""
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        self.__height = value
