655dfcd150e2fe6f9bdd4ec0c834ee22c5cff8f1
djshampz904 <djshampz05@gmail.com>
2024-02-05 20:12:56 +0300

    Fix shebang and add docstrings to Rectangle class

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,18 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+""" 
+Class that defines a rectangle
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     __width = 0
     __height = 0
     
     def __init__(self, width, height):
+        """ 
+        Instantiation with width and height: def __init__(self, width, height):
+        """
         super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
@@ -12,10 +20,19 @@ class Rectangle(BaseGeometry):
         self.__height = height
     
     def area(self):
+        """ 
+        Public instance method: def area(self): that returns the area of the rectangle
+        """
         return self.__width * self.__height
     
     def __str__(self):
+        """ 
+        Return the following rectangle description: [Rectangle] <width>/<height>
+        """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
     
     def __repr__(self):
+        """ 
+        Return the following rectangle description: [Rectangle] <width>/<height>
+        """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
