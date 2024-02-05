30a1da4a9ab85f6838dc2163368c518a37e1ceab
djshampz904 <djshampz05@gmail.com>
2024-02-05 20:34:07 +0300

    Fix import order in Square class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
 Class that defines a square
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
