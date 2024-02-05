077d6fe082f23a6d54e96097289d01d84a44fff8
djshampz904 <djshampz05@gmail.com>
2024-02-05 21:32:31 +0300

    Override int operators == and != in MyInt class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,19 +1,14 @@
+#!/usr/bin/python3
+"""this module defines a class MyInt that inherits from int"""
+
+
 class MyInt(int):
-    """
-    A class that inherits from int and inverts the == and != operators
+    """Invert int operators == and !="""
 
-    >>> my_int = MyInt(3)
-    >>> my_int == 3
-    False
-    >>> my_int != 3
-    True
-    >>> my_int == 4
-    True
-    >>> my_int != 4
-    False
-    """
-    def __eq__(self, other):
-        return int.__ne__(self, other)
+    def __eq__(self, value):
+        """Override == opeartor with != behavior"""
+        return self.real != value
 
-    def __ne__(self, other):
-        return int.__eq__(self, other)
+    def __ne__(self, value):
+        """Override != operator with == behavior"""
+        return self.real == value
