68396cfa2372f2a86b01888fdd585f2d979575a4
djshampz904 <martin.kimani@strathmore.edu>
2024-03-13 18:05:53 +0300

    slq

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT,
-    name VARCHAR(256),
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
