2569a94b2b97ebc4b255233431b0fdc1a0507bdf
djshampz904 <martin.kimani@strathmore.edu>
2024-04-13 14:38:09 +0300

    javascript is amazing

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const args = process.argv
+const args = process.argv;
 if (args.length === 2) {
-  console.log('No argument')
+  console.log('No argument');
 } else {
-    console.log(args[2])
+  console.log(args[2]);
 }
