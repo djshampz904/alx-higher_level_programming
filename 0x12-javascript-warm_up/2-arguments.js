5ab253937ae6a2e33e2c8022fd2ce44936f4dfb5
djshampz904 <martin.kimani@strathmore.edu>
2024-04-13 14:27:29 +0300

    javascript is amazing

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,6 @@
 const args = process.argv;
 if (args.length === 2) {
   console.log('No argument');
-}
-else if (args.length >= 3) {
+} else if (args.length >= 3) {
   console.log('Argument found');
 }
