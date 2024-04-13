48a6cd0f22992452b2d839493d23319cff0243a7
djshampz904 <martin.kimani@strathmore.edu>
2024-04-13 15:03:37 +0300

    javascript is amazing

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
 const args = process.argv.slice(2);
-console.log(parseInt(args[0]) + parseInt(args[1]));
+console.log(add(parseInt(args[0]), parseInt(args[1])));
