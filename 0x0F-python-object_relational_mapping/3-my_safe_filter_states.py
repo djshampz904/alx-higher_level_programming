c42a633d8860fb6e56e785367b3ad4ac03d50066
djshampz904 <martin.kimani@strathmore.edu>
2024-04-16 17:42:44 +0300

    orm

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-# script should take 4 arguments: mysql username, mysql password,
-# database name and state name searched(safe from MySQL injection)
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        passwd=sys.argv[2],
-        user=sys.argv[1],
-        db=sys.argv[3],
-        port=3306
-    )
-
-    searched = sys.argv[4]
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name LIKE %s",
-                (searched,))
-
-    for row in cur.fetchall():
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
     cur.close()
     db.close()
