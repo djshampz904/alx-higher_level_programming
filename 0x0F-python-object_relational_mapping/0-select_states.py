e95b953f792a60f2b43321338904bc863cd4b978
djshampz904 <martin.kimani@strathmore.edu>
2024-04-16 17:14:23 +0300

    orm

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
+
 
 db = MySQLdb.connect(
     host="localhost",
-    passwd="toor",
-    user="shvmpz",
-    db="hbtn_0e_0_usa"
+    passwd=sys.argv[2],
+    user=sys.argv[1],
+    db=sys.argv[3]
 )
 
 cur = db.cursor()
@@ -16,4 +18,5 @@ cur.execute("SELECT * FROM states ORDER BY id ASC")
 for row in cur.fetchall():
     print(row)
 
+cur.close()
 db.close()
