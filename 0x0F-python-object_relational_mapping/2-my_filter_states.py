7ae9e740a16b6161b8bb92f599cc9c0eed16ca18
djshampz904 <martin.kimani@strathmore.edu>
2024-04-16 17:29:00 +0300

    orm

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        passwd=sys.argv[2],
+        user=sys.argv[1],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    searched = sys.argv[4]
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' "
+                "ORDER BY states.id ASC"
+                .format(searched))
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
