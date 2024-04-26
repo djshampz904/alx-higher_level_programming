5be2a6744c4b34de2d314cecdc4bd43f4efd9d63
djshampz904 <martin.kimani@strathmore.edu>
2024-04-26 13:04:00 +0300

    orm

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script sends a request to a URL and displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f 2
+curl -s "$1" | wc -c
