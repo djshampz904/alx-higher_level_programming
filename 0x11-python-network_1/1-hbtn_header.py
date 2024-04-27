de57e0f099fb0ea646b00dea65ee5065fb95c915
djshampz904 <martin.kimani@strathmore.edu>
2024-04-27 15:22:01 +0300

    python networking 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""Fetches X-Request-Id from a URL"""
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header of the response.
+"""
 import sys
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.getheader('X-Request-Id'))
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
