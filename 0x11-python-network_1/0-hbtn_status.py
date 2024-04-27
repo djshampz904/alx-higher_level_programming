9a22a2c971a3b27fe27c9d9ce7c556712643ebe6
djshampz904 <martin.kimani@strathmore.edu>
2024-04-27 15:16:19 +0300

    python networking 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 
 if __name__ == "__main__":
-    """Fetches https://alx-intranet.hbtn.io/status"""
+    import urllib.request
+
     with (urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
           as response):
         html = response.read()
