26fe136f50439b6a6410d14c465fe9ac49e8b87f
djshampz904 <martin.kimani@strathmore.edu>
2024-04-27 15:23:20 +0300

    python networking 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,7 +9,5 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
